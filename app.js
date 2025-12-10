2bdaefb696566f382d2c91008b2eef33049b9fb4
JKjiwon <jwkim.oa@gmail.com>
2025-12-10 16:56:27 +0900

    Feat: 게이트 웨이를 통해 북서버 접속

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ recommendBtn.addEventListener('click', async () => {
 
   try {
     const response = await fetch(
-        `http://localhost:8081/api/v1/search/books?${params.toString()}`,
+        `http://localhost:8080/api/v1/search/books?${params.toString()}`,
         {
           method: 'GET',
           headers: {
